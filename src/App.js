404304c8fd98cc63c9826408742490c72b9831f0
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-21 22:18:13 -0500

    Styled the app

--- src/App.js
+++ src/App.js
@@ -41,19 +41,22 @@ const getCourseNumber = course => (
 );
 
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) } : { course.title }
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
   </div>
 );
 
 const CourseList = ({ courses }) => (
-  <div>
+  <div className="course-list">
     { Object.values(courses).map(course => <Course key={ course.id } course={ course } />) }
   </div>
 );
 
 const App = () => (
-  <div>
+  <div className="container">
     <Banner title={ schedule.title } />
     <CourseList courses={ schedule.courses } />
   </div>
