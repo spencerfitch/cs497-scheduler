66a9caf95137a23b6785be17f74eaf53ec2ad81c
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-21 21:49:06 -0500

    Changed page contents

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{ schedule.title }</h1>
     </div>
   );
 }
